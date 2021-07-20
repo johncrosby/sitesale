8221085633bde3149be8e309207eb2568b725607
John Crosby <johncrosby36@gmail.com>
2021-07-20 12:45:49 +0100

    updated Gen.Params.Static

--- src/Pages/ForSale.elm
+++ src/Pages/ForSale.elm
@@ -3,7 +3,7 @@ module Pages.ForSale exposing (page)
 -- import Gen.Route
 -- import Gen.Params.Static exposing (Params)
 
-import Gen.Params.Static exposing (Params)
+import Gen.Params.ForSale exposing (Params)
 import Html
 import Page exposing (Page)
 import Request
